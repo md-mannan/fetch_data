a9efc29a191e93f6954b797821fc7be660a1b879
md-mannan <mdmannan580@gmail.com>
2025-05-18 20:26:25 +0300

    get data by fetch api

--- src/app.js
+++ src/app.js
@@ -1,19 +1,9 @@
 function getRequest(url){
-
-    let xhr=new XMLHttpRequest()
-    xhr.open('get',url)
-
-    xhr.onreadystatechange=function(){
-        if(xhr.readyState===4){
-            if(xhr.status===200){
-                data=JSON.parse(xhr.response)
-                console.log(data)
-            }else{
-                console.log(xhr.status)
-            }
-        }
-    }
-    xhr.send()
+fetch(url)
+.then(response=>response.json())
+.then(json=>console.log(json))
+    
+    
 }
 let url='https://jsonplaceholder.typicode.com/users'
 getRequest(url)
